0e43d1bd8e47f99d764869deffef073602f6525a
LUKITA1416171822 <yehiahesham45@gmail.com>
2024-02-27 12:34:55 +0200

    Game Logic added with initial styling

--- Script.js
+++ Script.js
@@ -1,15 +1,27 @@
+
+let ComputerScore=0;
+let PlayerScore=0;
+document.getElementById("you").innerHTML += PlayerScore;
+document.getElementById("computer").innerHTML += ComputerScore;
 function getComputerChoice(){
-    let arr=["ROCK","PAPER","SISSORS"];
+    let arr=["ROCK","PAPER","SCISSORS"];
     return arr[Math.floor(Math.random()*3)];
 }
 function result(player_Selection){
     let Computer=getComputerChoice();
     let player = player_Selection.toUpperCase();
     if(Computer==player)
-        return "Draw";
-    else if(Computer=="ROCK"&& player=="SISSORS"|| Computer=="SISSORS"&& player=="ROCK" || Computer=="PAPER"&& player=="ROCK")
-        return "Computer Wins";
-    else 
-    return "Player Wins";
+       { return ;}
+    else if(Computer=="ROCK" && player=="SCISSORS"|| Computer=="SCISSORS"&& player=="PAPER" || Computer=="PAPER"&& player=="ROCK")
+        {
+        ComputerScore++;}
+    else {
+    PlayerScore++;}
+    updateScores();
+   // alert('Computer choice : '+Computer+'\nYour choice: '+player+'\nResult: '+Computer+' '+((Computer==player)?"=":(Computer=="ROCK"&& player=="SISSORS"|| Computer=="SISSORS"&& player=="ROCK" || Computer=="PAPER"&& player=="ROCK")?"WINS":"LOSES"));
 }
-alert(result("ROck"))
+function updateScores() {
+    document.getElementById("you").innerHTML = "you: " + PlayerScore;
+    document.getElementById("computer").innerHTML = "computer: " + ComputerScore;
+}
+// alert(result("ROck"))
